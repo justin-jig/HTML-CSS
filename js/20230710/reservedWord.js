6eb79094028e5e82a4f47ff8eadf5a839895e2f0
justin-jig <justin-j@naver.com>
2023-07-11 14:30:07 +0900

    study:20230711 js&css 공부

---
+++ js/20230710/reservedWord.js
@@ -0,0 +1,36 @@
+/**
+    
+    예약어  
+    특별한 의미를 가지고 있어, 변수나 함수 이름 등으로 사용할 수 없는 단어
+
+*/ 
+
+
+/** 자료형을 알려주는 typeof */
+
+// console.log(typeof "안녕");
+// console.log(typeof 3);
+// console.log(typeof null); // null => object 자료형
+
+/** promot */
+// let score1 = prompt("첫번째 선정을 입력하세요.");
+// let score2 = prompt("두번째 선정을 입력하세요.");
+// console.log(score1, score2);
+// let avg = (Number(score1) + Number(score2)) / 2;
+// console.log(avg)
+
+/** String, toString 문자열로 형변한 */
+// let num = 5;
+// console.log(String(num));
+// console.log(num.toString());
+
+/** Number 숫자형으로 형변환 */
+// let string = "5";
+// console.log(Number(string));
+
+
+
+
+
+
+
