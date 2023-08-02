563db6b0ed01d39bed7cd912005ad1298d72538d
justin-jig <justin-j@naver.com>
2023-08-02 16:23:06 +0900

    20230801 node study

---
+++ node/study/study/dynamicForm/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+//body-parser
+app.use(express.urlencoded({extended: true})); // url 인코딩 방식일 때
+app.use(express.json()); // json 방식일 때
+
+//view engine
+app.set("view engine", 'ejs');
+app.set('views', './views');
+
+//router
+app.get('/', (req, res) => {
+    res.render('dynamicForm');
+})
+
+app.get('/ajax', (req, res) => {
+    console.log('back', req.query);
+    res.send(req.query);
+})
+app.post('/ajax', (req, res) => {
+    console.log('back', req.body);
+    res.send(req.body);
+})
+
+app.get('/axios', (req, res) => {
+    console.log('back', req.query);
+    res.send(req.query);
+})
+
+app.post('/axios', (req, res) => {
+    console.log('back', req.body);
+    res.send(req.body);
+})
+
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+})
