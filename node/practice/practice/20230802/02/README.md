2627310ee2144c9d8b9d154127c790baf987de2b
justin-jig <justin-j@naver.com>
2023-08-02 19:02:47 +0900

    20230802 node express.js axios study

---
+++ node/practice/practice/20230802/02/README.md
@@ -0,0 +1,5 @@
+# KDT-WEB-9 20230802
+
+node 실습 코드 02
+
+사용 기능 :  middleware, express
---
+++ node/practice/practice/20230802/02/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const PROT = 8000;
+
+// set() : server에 속성을 셋팅
+// ejs setting
+
+app.use(express.urlencoded({extended: true})); // url 인코딩 방식일 때
+app.use(express.json()); // json 방식일 때
+
+app.set("view engine", 'ejs');
+app.set('views', './views');
+
+// express get 함수
+app.get('/', (req, res) => {
+    res.render('form', {title :'axios로 로그인'});
+})
+
+// express post 함수
+app.post('/login', (req, res) => {
+    
+    let id = 'test1234'
+    let pw = '1234';
+
+    console.log(req.body)
+
+    if ( req.body.id != id ) { 
+
+        res.send({ resultcode:'01', result : '없는 id입니다.'});
+        
+    } else if ( req.body.pw != pw ) { 
+
+        res.send({ resultcode:'02', result : '비밀번호가 틀립니다.'});
+        
+    } else {
+
+        res.send({ resultcode:'03', result : '로그인이 되었습니다.'});
+    }
+
+})
+
+/** 서버 열어주는 함수 */
+app.listen(PROT, () => {
+    console.log(`http://localhost:${PROT}`);
+})
+
